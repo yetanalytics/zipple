1dc892f44866890a17f121d00c126b109593f1be
Milt Reder <milt@anestuary.com>
2016-08-10 21:18:35 -0400

    copyright and proj info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.yetanalytics/zipple "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Some simple zip utils"
+  :url "http://github.com/yetanalytics/zipple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
